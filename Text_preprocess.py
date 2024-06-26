a52d3f81fe5e7047156723276a956e9114212afb
akkirajubhavana <141499613+akkirajubhavana@users.noreply.github.com>
2024-06-26 23:44:43 +0530

    Update Text_preprocess.py
    Modified the punctuation logic

--- Text_preprocess.py
+++ Text_preprocess.py
@@ -14,41 +14,39 @@ class UnicodeRangeChecker:
         self.regex_patterns = {
             lang: f"[{''.join(ranges)}]+" for lang, ranges in unicode_ranges.items()
         }
-        self.punctuation_pattern = self.create_punctuation_pattern()
 
-    def create_punctuation_pattern(self):
+    def create_punctuation_pattern(self, lang):
         general_punctuations = ''.join(self.punctuations.get('general', []))
-        arabic_punctuations = ''.join(self.punctuations.get('arabic', []))
         hyphens = ''.join(self.punctuations.get('hyphens', []))
-        punctuation_pattern = f"[{general_punctuations}{arabic_punctuations}{hyphens}]"
-        return re.compile(punctuation_pattern)
+        punctuation_pattern = general_punctuations + hyphens
+        if lang in ['ar', 'ur', 'fa']:
+            arabic_punctuations = ''.join(self.punctuations.get('arabic', []))
+            punctuation_pattern += arabic_punctuations
+        return re.compile(f"[{punctuation_pattern}]")
     
-    def remove_punctuations(self, text):
-        return self.punctuation_pattern.sub('', text)
+    def remove_punctuations(self, text, lang):
+        punctuation_pattern = self.create_punctuation_pattern(lang)
+        return punctuation_pattern.sub('', text)
 
     def in_unicode_range(self, text, lang):
         if lang not in self.regex_patterns:
             print(f"Language {lang} not supported.")
             return
         
-        text = self.remove_punctuations(text)
+        text = self.remove_punctuations(text, lang)
         regex_pattern = self.regex_patterns[lang]
         matching_characters = re.findall(regex_pattern, text)
         non_matching_characters = re.findall(f"[^{regex_pattern[1:-2]}]", text)
         
-        if matching_characters:
-            matching_chars = ' '.join(matching_characters)
-        else:
-            matching_chars = "NAAN"
-        
-        if non_matching_characters:
-            non_matching_chars = ' '.join(non_matching_characters)
-        else:
-            non_matching_chars = "NANA "
+        matching_chars = ' '.join(matching_characters)
+        non_matching_chars = ' '.join(non_matching_characters)
         
         return matching_chars, non_matching_chars
 
-def main(yaml_file, input_file, output_file, language):
+def contains_english(text):
+    return bool(re.search(r'[A-Za-z0-9]', text))
+
+def main(yaml_file, input_file, output_file, separate_file, language):
     # Load the YAML configuration file
     with open(yaml_file, 'r', encoding='utf-8') as file:
         config = yaml.safe_load(file)
@@ -60,25 +58,34 @@ def main(yaml_file, input_file, output_file, language):
     # Create an instance of the UnicodeRangeChecker
     checker = UnicodeRangeChecker(unicode_ranges, punctuations)
 
-    # Read text from the input file and write to the output file
-    with open(input_file, 'r', encoding='utf-8') as text_file, open(output_file, 'w', encoding='utf-8') as output_file:
+    # Read text from the input file and write to the respective output files
+    with open(input_file, 'r', encoding='utf-8') as text_file, \
+         open(output_file, 'w', encoding='utf-8') as output_file, \
+         open(separate_file, 'w', encoding='utf-8') as sep_file:
+        
         for line in text_file:
-            columns = line.strip().split('\t')
+            columns = line.strip().split(' ',1)
             if columns:
                 text = columns[1]  # Assuming text is in the second column
                 matching_chars, non_matching_chars = checker.in_unicode_range(text, language)
-                output_file.write(f"{columns[0]}\t{matching_chars}\n")
+                # print(len(non_matching_chars))
+                if contains_english(text):
+                    sep_file.write(f"{columns[0]}\t{text}\n")
+                else:
+                    output_file.write(f"{columns[0]}\t{matching_chars}\n")
 
 if __name__ == "__main__":
     # Check if the correct number of command-line arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <yaml_file> <input_file> <output_file> <language>")
+    if len(sys.argv) != 6:
+        print("Usage: python script.py <yaml_file> <input_file> <output_file> <separate_file> <language>")
         sys.exit(1)
 
     # Extract command-line arguments
     yaml_file = sys.argv[1]
     input_file = sys.argv[2]
     output_file = sys.argv[3]
-    language = sys.argv[4]
+    separate_file = sys.argv[4]
+    language = sys.argv[5]
 
-    main(yaml_file, input_file, output_file, language)
+    main(yaml_file, input_file, output_file, separate_file, language)
+
